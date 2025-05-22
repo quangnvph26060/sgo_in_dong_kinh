459c53af6bc40d5235baeb222f347a20ac5554e0
datntdev968 <datntdev968@gmail.com>
2025-05-22 21:54:22 +0700

    update code

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -14,6 +14,8 @@
     @include('frontend.includes.style')
 
     <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v22.0&appId=598605722761627"></script>
 
 </head>
 
